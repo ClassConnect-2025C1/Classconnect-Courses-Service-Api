96234dbe7c2b43011214347c0dd6031436273f09
Igris-1 <marcelo_n97@outlook.es>
2025-05-25 18:48:38 -0300

    Remove commented tasks from enrollment model

--- internal/model/enrollment.go
+++ internal/model/enrollment.go
@@ -7,10 +7,3 @@ type Enrollment struct {
 	CourseID uint   `json:"course_id" gorm:"index"`
 	Favorite bool   `json:"favorite" gorm:"default:false"`
 }
-
-// (1 - terminado) Modificar el response de los cursos aprobados para que devuelva el nombre del curso
-// (2 - terminado) modificar el critterio de elegibilidad que sea una lista de strings (modificar el edit)
-
-// (3 - terminado) implementar los cursos favoritos (modificar el enrollment para que contenga un bool)
-// (4 - shit) Chequear al momento del enrollment que el usuario cumpla con los criterios
-// (4 - terminado) chequear que el usuario inscripto realmente este inscripto al momento de ponerlo en favorito
